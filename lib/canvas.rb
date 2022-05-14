e8460313c66d0ec12f813b4969150cb2081ff86d
sachimp <sachingandhi@live.com>
2022-05-14 03:19:32 +0100

    prototyped methods in canvasfile for some reason

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -1,13 +1,39 @@
 class Canvas
-  attr_accessor :height, :width
+  attr_accessor :height, :width, :canvas
 
   def initialize(height, width)
     @height = height
     @width = ("O" * width).chars
+    @canvas = [@width] * @height
   end
 
   def build
-    [@width] * @height
+    @canvas.each do |row|
+      puts row.join
+    end
   end
 
 end
+
+test = Canvas.new(3, 9)
+
+# default blank
+test.build
+puts ''
+
+# colour a column
+test.canvas.map do |row|
+  row[2] = "c"
+end
+test.build
+puts ''
+
+# colour a row
+test.canvas[1] = ("r" * test.width.count).chars
+test.build
+puts ''
+
+# colour one pixel
+test.canvas[1][3] = "p"
+test.build
+puts ''
